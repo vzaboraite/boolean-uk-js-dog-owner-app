b36a96d7cffe74d057480f2d38a14ef2d13cebc7
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-23 23:30:52 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -7,14 +7,17 @@ const data = [
             eum maxime illo laborum omnis quo ab rem cupiditate nulla
             perspiciatis ipsum!`,
     isGoodDog: true,
-    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_1.jpg"
+    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_1.jpg",
   },
   {
     id: 2,
     name: "Nugget",
-    bio: `Bio 2`,
+    bio: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum,
+            minima voluptates libero cumque rerum consequatur optio aliquid sint
+            eum maxime illo laborum omnis quo ab rem cupiditate nulla
+            perspiciatis ipsum!`,
     isGoodDog: false,
-    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_2.jpg"
+    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_2.jpg",
   },
   {
     id: 3,
@@ -24,7 +27,7 @@ const data = [
             eum maxime illo laborum omnis quo ab rem cupiditate nulla
             perspiciatis ipsum!`,
     isGoodDog: true,
-    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_3.jpg"
+    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_3.jpg",
   },
   {
     id: 4,
@@ -34,7 +37,7 @@ const data = [
             eum maxime illo laborum omnis quo ab rem cupiditate nulla
             perspiciatis ipsum!`,
     isGoodDog: false,
-    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_4.jpg"
+    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_4.jpg",
   },
   {
     id: 5,
@@ -44,7 +47,7 @@ const data = [
             eum maxime illo laborum omnis quo ab rem cupiditate nulla
             perspiciatis ipsum!`,
     isGoodDog: false,
-    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_5.jpg"
+    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_5.jpg",
   },
   {
     id: 6,
@@ -54,7 +57,7 @@ const data = [
             eum maxime illo laborum omnis quo ab rem cupiditate nulla
             perspiciatis ipsum!`,
     isGoodDog: false,
-    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_6.jpg"
+    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_6.jpg",
   },
   {
     id: 7,
@@ -64,8 +67,8 @@ const data = [
             eum maxime illo laborum omnis quo ab rem cupiditate nulla
             perspiciatis ipsum!`,
     isGoodDog: true,
-    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_7.jpg"
-  }
+    image: "https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_7.jpg",
+  },
 ];
 
 window.data = data;
